398be7bca9157c3177d32ff12e837f572a3294c0
bsongkoputrogo-atlassian <bsongkoputrogo@atlassian.com>
2026-03-09 10:58:09 +1300

    ABC-123: Update insecure-code.js

--- insecure-code.js
+++ insecure-code.js
@@ -11,3 +11,15 @@ function runUserCommand(userInput) {
         console.log(`Output: ${stdout}`);
     });
 }
+
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database(':memory:');
+
+// User input from a query parameter
+const userId = req.query.id;
+
+// TRIGGER: Direct concatenation of untrusted input into a SQL query
+db.get("SELECT * FROM users WHERE id = " + userId, (err, row) => {
+  if (err) throw err;
+  console.log(row);
+});
